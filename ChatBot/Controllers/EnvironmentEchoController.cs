c7532c119633d285abbee4605094a9af43666699
sebug <sebastian.gfeller@gmail.com>
2017-08-19 13:42:00 +0200

    Initial Commit

---
+++ ChatBot/Controllers/EnvironmentEchoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class EnvironmentEchoController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Return Bot App ID and secret from environment variables here.";
+        }
+    }
+}
